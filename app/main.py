18c400e932cbe761422e03e6359acbfff8f790b2
michaelheyman <mitch.heyman@gmail.com>
2019-11-10 10:56:06 -0800

    CF1-7 Store and query data from database

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 import pandas as pd
 
+from app import settings
+from app.database import dal
+from app.database import query
+
 
 def run():
     """Entry point for the application.
 
-    :return: DataFrame representation of a CSV file
+    Loads CSV faculty data and stores it into a database.
+
+    :return: Count of faculty members and list of faculty names
     """
-    return pd.read_csv("data/faculty.csv")
+    dal.db_init()
+    df = pd.read_csv(settings.DATA_CSV)
+    df.to_sql("Faculty", con=dal.engine, index_label="FacultyId", if_exists="replace")
+
+    faculty_count = query.faculty_count()
+    faculty_names = query.faculty_names()
+
+    dal.DBSession.close()
+    return faculty_count, faculty_names
